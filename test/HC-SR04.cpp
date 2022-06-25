a0d2d2342eadd9f62351c9643b1544f61217bd8a
TPAXcc <91980905+TPAXcc@users.noreply.github.com>
2022-06-25 12:50:12 +0800

    Add files via upload

---
+++ test/HC-SR04.cpp
@@ -0,0 +1,32 @@
+//2022.6.25 12:29 Ver.0.1.0.alpha 初始版本
+//TPAXcc Form github.com/TPAXcc
+//future:
+//回传带有时间戳的数据（可定义）
+//回传收敛后的数据（可定义）
+//添加注释
+
+#if ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
+
+#include "HC-SR04.h"
+
+SR04::SR04(int TP, int EP) {
+    pinMode(EP,INPUT);
+    pinMode(TP,OUTPUT);
+    Echo = EP;
+    Trig = TP;
+}
+
+float SR04::Get() {
+    digitalWrite(Trig,LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trig,LOW);
+
+    distance = pulseIn(Echo, HIGH)/58;
+    return distance;
+}
